4089981172dd94ba75a9985cd9ae95d44a67c5a5
vickypineli <v.pinero70@gmail.com>
2023-03-08 10:22:34 +0100

    agregar fotos fake

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,30 @@
 import React from "react";
+// import { ArrowRightIcon } from "@heroicons/react/solid";
 
 export default function Navbar() {
-    <h1>
-    Barra de navegacion
-      </h1>
+    <nav class="navbar navbar-expand-md navbar-light">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-toggler">
+          {/* <a class="navbar-brand" href="#">
+            <img src="imagenes/icono.png" width="50" alt="Logo de la página web">
+          </a> */}
+          <ul class="navbar-nav d-flex justify-content-center align-items-center">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page"href="#about" >Sobre mí</a>
+            </li>
+   
+            <li class="nav-item">
+              <a class="nav-link" href="#testimonials">Testimonios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contacto</a>
+              
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 }
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Group Project: Football Pickem' League",
+    subtitle: "Chart.js, Sequelize, Express-Handlebars",
+    description:
+      "My classmates and I built a weekly college football league that uses chart.js to display a scoreboard!",
+    image: "./cfb_scoreboard.png",
+    link: "https://safe-sands-42348.herokuapp.com/",
+  },
+  {
+    title: "Group Project: Astrology App",
+    subtitle: "jQuery & Foundation",
+    description:
+      "My classmates and I built a neat astrology app that uses two APIs to give a daily horoscope, among other things!",
+    image: "./projectOne_Screencap.png",
+    link: "https://bjbci.github.io/projectOne/",
+  },
+  {
+    title: "Just Another Text Editor",
+    subtitle: "Service Workers & Progressive Web Applications",
+    description:
+      "In this project I built a progressive web applicaiton that can run offline!",
+    image: "./text_cat.PNG",
+    link: "https://radiant-eyrie-05038.herokuapp.com/",
+  },
+  {
+    title: "Original Portfolio",
+    subtitle: "HTML & CSS",
+    description:
+      "This is a very early project I worked on in the bootcamp, in which I made a portfolio with just raw HTML and CSS!",
+    image: "./Challenge_2_Screencapture.png",
+    link: " https://hunter-mayer.github.io/homework-2/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Hunter is pretty cool I guess. He gives us lots of cat treats.",
+    image: "./my_cats.jpg",
+    name: "Geralt & Jaskier",
+    company: "",
+  },
+  {
+    quote:
+      "Best. Fiancé. EVER!",
+    image: "./alyshia.jpg",
+    name: "Alyshia Greene",
+    company: "",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "MongoDB",
+  "Node",
+  "GraphQL",
+  "Express",
+];
